2f5c7c60d92d816398e983e881ef230535e055dc
Ahmed-20021998 <158044760+Ahmed-20021998@users.noreply.github.com>
2025-04-25 01:24:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import google.generativeai as genai
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 
 # Configure Gemini
